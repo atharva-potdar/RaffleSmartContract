f9b1f3be4e0c2baca9a0aa39ab428d853b919be5
atharva-potdar <atharvapotdar07@gmail.com>
2025-11-05 08:56:40 +0530

    feat: add license and version to integration test file

--- test/integration/Interactions.t.sol
+++ test/integration/Interactions.t.sol
@@ -1,2 +1,6 @@
 // Write integration tests to get code coverage to 100% for scripts/Interactions.s.sol
 // Also for test/mocks/LinkToken.sol if needed
+
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.30;
